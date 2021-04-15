ae32e4ca081e181e7bbc99126d0f8a60df5a0c8c
ConradMeyer <meyeralten@gmail.com>
2021-04-15 14:30:39 +0200

    Ahora si

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.get("/search/:localization/:keyword", async (req,res) => {
     
     const finalResult = [...result, ...result2];
 
-    if (req.headers.authorization || !req.headers.authorization === null) {
+    if (req.headers.authorization && !req.headers.authorization === null) {
         const favoritos = await readFav(req.headers.authorization);
 
         const compararFinal = finalResult.map(el => {
